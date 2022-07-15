b1253ae72b5cc922ff928d14b3892bb6d8c037b9
CalB158 <calzadab522@gmail.com>
2022-07-15 01:08:25 -0500

    Updated

--- Post.py
+++ Post.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Student(BaseModel):
+    name: str
+    lastname: str
+    skills: list
+
 @app.post("/students")
-def saveStudent(name: str, lastname: str):
-    return f"Estudiante {name} {lastname} guardado!"
+def saveStudent(student: Student):
+    return f"Habilidades de {student.name}: {student.skills}"
